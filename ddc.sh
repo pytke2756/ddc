2b6c98abcf1b3093c16f9d931527f824273c2128
pytke2756 <ibranyilevente@gmail.com>
2024-03-07 18:50:57 +0100

    add custom port

--- ddc.sh
+++ ddc.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-while getopts u:p:d:c: flag; 
+while getopts u:p:d:c:t: flag;
 do
   case ${flag} in
     u) USER="$OPTARG";;
     p) PASSWORD="$OPTARG";;
     d) DB_NAME="$OPTARG";;
     c) CONTAINER_NAME="$OPTARG";;
+    t) PORT="$OPTARG";;
+    *) ;;
   esac
 done
 
@@ -14,13 +16,19 @@ echo "user: $USER";
 echo "password: $PASSWORD";
 echo "db-name: $DB_NAME-db";
 echo "container-name: $CONTAINER_NAME"
+echo "port: $PORT"
 
 CURRENT_DATE=$(date +"%Y-%m-%d")
 CONTAINER_WITH_TIME="${CONTAINER_NAME}-${CURRENT_DATE}"
 
 docker exec -it ${CONTAINER_NAME} psql -U ${USER} -d ${DB_NAME} -c "INSERT INTO kor (id, ember_id, kor) VALUES ('2', '2', '4444');"
 
-docker create --name ${CONTAINER_WITH_TIME}  -e POSTGRES_USER=${USER} -e POSTGRES_PASSWORD=${PASSWORD} -e POSTGRES_DB=${DB_NAME} -p '5888:5432' db
+if [ -z "$PORT" ]; then
+  docker create --name ${CONTAINER_WITH_TIME}  -e POSTGRES_USER=${USER} -e POSTGRES_PASSWORD=${PASSWORD} -e POSTGRES_DB=${DB_NAME} -p "5888:5432" db
+else
+  docker create --name ${CONTAINER_WITH_TIME}  -e POSTGRES_USER=${USER} -e POSTGRES_PASSWORD=${PASSWORD} -e POSTGRES_DB=${DB_NAME} -p "${PORT}:5432" db
+fi
+
 sleep 5
 docker exec -it ${CONTAINER_NAME} pg_dump -U ${USER} -d ${DB_NAME} -Fc -f db.dump && docker cp ${CONTAINER_NAME}:db.dump .
 sleep 5
