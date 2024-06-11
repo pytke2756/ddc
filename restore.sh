2e7a97b2b6578aa00a08c309499e5abdbc7c4f07
pytke2756 <ibranyilevente@gmail.com>
2024-06-11 08:52:07 +0200

    restore use source

--- restore.sh
+++ restore.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 while getopts u:d:c:p:s: flag;
 do
@@ -7,19 +7,32 @@ do
     d) DB_NAME="$OPTARG";;
     c) CONTAINER_NAME="$OPTARG";;
     p) PORT="$OPTARG";;
-    s) SOURCE="$OPTARG";;
     *);;
   esac
 done
+
+if [ -z $PORT ]; then
+  PORT="5432"
+fi
+
+shift $((OPTIND -1))
+
+SOURCE=$1
+
+if [[ $SOURCE =~ /([^/]+)\.([^/]+[dump])$ ]]; then
+  FILE_NAME=${BASH_REMATCH[1]}.${BASH_REMATCH[2]}
+else
+  SOURCE="./db.dump"
+  FILE_NAME="db.dump"
+fi
 
 echo "user: $USER";
 echo "db-name: $DB_NAME";
 echo "container-name: $CONTAINER_NAME";
 echo "port: $PORT";
-echo "source: $SOURCE (TODO)";
+echo "source: $SOURCE";
+echo "file-name: $FILE_NAME";
 
-if [ -z "$PORT" ]; then
-  docker cp db.dump ${CONTAINER_NAME}:/ && docker exec -it ${CONTAINER_NAME} pg_restore --if-exists -c -U ${USER} -d ${DB_NAME} db.dump
-else
-  docker cp db.dump ${CONTAINER_NAME}:/ && docker exec -it ${CONTAINER_NAME} pg_restore -p '5432' --if-exists -c -U ${USER} -d ${DB_NAME} db.dump
-fi
+docker cp ${SOURCE} ${CONTAINER_NAME}:/ && docker exec -it ${CONTAINER_NAME} pg_restore -p ${PORT} --if-exists -c -U ${USER} -d ${DB_NAME} ${FILE_NAME}
+
+exit 1
